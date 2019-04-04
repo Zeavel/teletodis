8e2229bb3b102526f6466ab5e6c980622aef807f
Zeavel <34531943+Zeavel@users.noreply.github.com>
2019-04-04 19:27:29 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,6 +156,7 @@ if(msg.text.startsWith("/add"))
 {
     console.log("da")
     var ide = msg.text.substring(5)
+    console.log(ide)
    client2.channels.get(ide).createWebhook(client2.channels.get(ide).name, 'https://i.imgur.com/mI8XcpG.jpg')
     .then(webhook => {
      console.log(client2.user.username)
